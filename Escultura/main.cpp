c75354e38e1a152da758c9664cc794433fc56e04
EmanoelBatista <emanoel.0095@hotmail.com>
2022-06-22 07:45:04 -0300

    Atualização na main

--- Escultura/main.cpp
+++ Escultura/main.cpp
@@ -1,7 +1,62 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+#include <vector>
+#include "sculptor.h"
+#include "figurageometrica.h"
+#include "putvoxel.h"
+#include "cutvoxel.h"
+#include "putbox.h"
+#include "cutbox.h"
+#include "putsphere.h"
+#include "cutsphere.h"
+#include "putellipsoid.h"
+#include "cutellipsoid.h"
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    return a.exec();
+using namespace std;
+
+int main(){
+    ifstream fin;
+    string s;
+    stringstream ss;
+    vector <FiguraGeometrica*> figura;
+
+    fin.open("ect2.off");
+
+    if(!fin.is_open()){
+        exit(0);
+    }
+
+    int nx, ny, nz;
+
+    getline(fin, s);
+    ss.clear();
+    ss.str(s);
+    ss >> s;
+    ss >> nx >> ny >> nz;
+
+
+    Sculptor t(nx, ny, nz);
+
+
+
+
+
+
+
+
+
+
+
+    fin.close();
+
+    for(int i=0; i<int(figura.size()); i++){
+        figura[i]->draw(t);
+    }
+
+    t.writeOFF((char*)"ect2.off");
+    return 0;
 }
